cf2d8a9b6da21d9c59f1509ba0945d359d2694c0
David Blas Vázquez <david.bv@gmail.com>
2017-09-26 16:05:58 +0200

    C51Test

---
+++ src/test/java/david/blas/SVC/miw/upm/es/c5/C51Test.java
@@ -0,0 +1,28 @@
+package david.blas.SVC.miw.upm.es.c5;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C51Test {
+
+private C51 c51;
+    
+    @Before
+	public void before () {
+    	c51 = new C51();
+	}
+	
+	@Test
+	public void testM1() {
+		assertEquals("m1",c51.m1());
+	}
+
+	
+	@Test
+	public void testM2() {
+		assertEquals("m2",c51.m2());
+	}
+
+}
