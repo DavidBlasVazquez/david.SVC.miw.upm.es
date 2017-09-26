33c57babd771037fbb5ea738203f8683f9194421
David Blas Vázquez <david.bv@gmail.com>
2017-09-26 15:19:34 +0200

    C32Test

---
+++ src/test/java/david/blas/SVC/miw/upm/es/c3/C32Test.java
@@ -0,0 +1,21 @@
+package david.blas.SVC.miw.upm.es.c3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C32Test {
+	private C32 c32;
+	
+	@Before
+	public void before () {
+		c32 = new C32();
+	}
+	
+	@Test
+	public void testMA() {
+		assertEquals("mA",c32.mA());
+	}	
+
+}
